8a2bd671fe30e15594e622559e9f4a48ee67214e
Brittany Shannon <brittany.shannon@willowtreeapps.com>
2020-11-16 13:52:59 -0500

    Making code better and update dependencies

---
+++ app/src/main/java/com/bunny/android/lotterypicker/GameNumbers.java
@@ -0,0 +1,130 @@
+package com.bunny.android.lotterypicker;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class GameNumbers extends ArrayList<Integer> {
+
+    public ArrayList<Integer> setPick3() {
+        ArrayList<Integer> gameList = new ArrayList<>();
+        for (int count = 1; count <= 3; count++) {
+            Random randomNum = new Random();
+            int selectedNum = randomNum.nextInt(9);
+            gameList.add(selectedNum);
+            Collections.sort(gameList);
+        }
+        return gameList;
+    }
+
+    public ArrayList<Integer> setPick4() {
+        ArrayList<Integer> gameList = new ArrayList<>();
+        for (int count = 1; count <= 4; count++) {
+            Random randomNum = new Random();
+            int selectedNum = randomNum.nextInt(9);
+            gameList.add(selectedNum);
+            Collections.sort(gameList);
+        }
+        return gameList;
+    }
+
+    public ArrayList<Integer> setCash5() {
+        ArrayList<Integer> gameList = new ArrayList<>();
+        for (int count = 1; count <= 5; count++) {
+            Random randomNumber = new Random();
+            int selectedNum = randomNumber.nextInt(34) + 1;
+            boolean sameNumber = gameList.contains(selectedNum);
+            if (!sameNumber) {
+                gameList.add(selectedNum);
+            } else {
+                count--;
+            }
+            Collections.sort(gameList);
+        }
+        return gameList;
+    }
+
+    public ArrayList<Integer> setPowerBall() {
+        ArrayList<Integer> gameList = new ArrayList<>();
+        for (int count = 1; count <= 6; count++) {
+            if (count == 6) {
+                Random lastPick = new Random();
+                int powerBallPick = lastPick.nextInt(26) + 1;
+                gameList.add(powerBallPick);
+                break;
+            }
+            Random randomNumber = new Random();
+            int selectedNum = randomNumber.nextInt(69) + 1;
+            boolean sameNumber = gameList.contains(selectedNum);
+            if (!sameNumber) {
+                gameList.add(selectedNum);
+            } else {
+                count--;
+            }
+            Collections.sort(gameList);
+        }
+        return gameList;
+    }
+
+    public ArrayList<Integer> setMegaMillion() {
+        ArrayList<Integer> gameList = new ArrayList<>();
+        for (int count = 1; count <= 6; count++) {
+            if (count == 6) {
+                Random lastPick = new Random();
+                int megaBallPick = lastPick.nextInt(25) + 1;
+                gameList.add(megaBallPick);
+                break;
+            }
+            Random randomNum = new Random();
+            int selectedNum = randomNum.nextInt(70) + 1;
+            boolean sameNumber = gameList.contains(selectedNum);
+            if (!sameNumber) {
+                gameList.add(selectedNum);
+            } else {
+                count--;
+            }
+            Collections.sort(gameList);
+        }
+        return gameList;
+    }
+
+    public ArrayList<Integer> setCash4Life() {
+        ArrayList<Integer> gameList = new ArrayList<>();
+        for (int count = 1; count <= 6; count++) {
+            if (count == 6) {
+                Random lastPick = new Random();
+                int cashBallPick = lastPick.nextInt(4) + 1;
+                gameList.add(cashBallPick);
+                break;
+            }
+            Random randomNum = new Random();
+            int selectedNum = randomNum.nextInt(60) + 1;
+            boolean sameNumber = gameList.contains(selectedNum);
+            if (!sameNumber) {
+                gameList.add(selectedNum);
+            } else {
+                count--;
+            }
+            Collections.sort(gameList);
+        }
+        return gameList;
+    }
+
+    public ArrayList<Integer> setBankAmillion() {
+        ArrayList<Integer> gameList = new ArrayList<>();
+        for (int count = 1; count <= 6; count++) {
+            Random randomNum = new Random();
+            int selectedNum = randomNum.nextInt(40) + 1;
+            boolean sameNumber = gameList.contains(selectedNum);
+            if (!sameNumber) {
+                gameList.add(selectedNum);
+            } else {
+                count--;
+            }
+            Collections.sort(gameList);
+        }
+        return gameList;
+    }
+
+}
+
--- app/src/main/java/com/bunny/android/lotterypicker/MainActivity.java
+++ app/src/main/java/com/bunny/android/lotterypicker/MainActivity.java
@@ -1,9 +1,7 @@
 package com.bunny.android.lotterypicker;
 
 
-
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,6 +10,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
@@ -36,21 +36,23 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         theTIME = findViewById(R.id.timeField);
         loadActivity();
 
-        adapter = ArrayAdapter.createFromResource(getApplicationContext(),R.array.game_choices,R.layout.spinner_style);
-        spinner= findViewById(R.id.whichGame);
+        adapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.game_choices, R.layout.spinner_style);
+        spinner = findViewById(R.id.whichGame);
         adapter.setDropDownViewResource((android.R.layout.simple_spinner_dropdown_item));
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
     }
+
     @Override
-    public void onItemSelected(AdapterView<?> parent,View view, int position,long id){
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
     }
 
-    public void onNothingSelected(AdapterView<?> arg){
+    public void onNothingSelected(AdapterView<?> arg) {
 
     }
-    public void loadActivity(){
+
+    public void loadActivity() {
         TextView showNumber1 = findViewById(R.id.whereNumberShows1);
         showNumber1.setVisibility(View.GONE);
         TextView showNumber2 = findViewById(R.id.whereNumberShows2);
@@ -67,7 +69,7 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         lastBankBall.setVisibility(View.GONE);
     }
 
-    public void displayDaysAndTimes(){
+    public void displayDaysAndTimes() {
         StringBuilder forDays = new StringBuilder();
         forDays.append(getString(R.string.drawDays));
         forDays.append(gameDays);
@@ -81,9 +83,10 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         theTIME.setTextColor(Color.RED);
         theTIME.setTextSize(18);
     }
+
     public Integer whichGame() {
         loadActivity();
-         getGame = spinner.getSelectedItemPosition();
+        getGame = spinner.getSelectedItemPosition();
 
         return getGame;
     }
@@ -92,22 +95,19 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         displayList();
         displayDaysAndTimes();
         theList = null;
-        theList = new gameNumbers();
+        theList = new GameNumbers();
         getGame = null;
     }
 
-    public void showFirstThree(){
+    public void showFirstThree() {
         TextView showNumber1 = findViewById(R.id.whereNumberShows1);
         TextView showNumber2 = findViewById(R.id.whereNumberShows2);
         TextView showNumber3 = findViewById(R.id.whereNumberShows3);
-        int firstNumber = theList.get(0);
-        showNumber1.setText(String.valueOf(firstNumber));
+        showNumber1.setText(String.valueOf(theList.get(0)));
         showNumber1.setVisibility(View.VISIBLE);
-        int secondNumber = theList.get(1);
-        showNumber2.setText(String.valueOf(secondNumber));
+        showNumber2.setText(String.valueOf(theList.get(1)));
         showNumber2.setVisibility(View.VISIBLE);
-        int thirdNumber = theList.get(2);
-        showNumber3.setText(String.valueOf(thirdNumber));
+        showNumber3.setText(String.valueOf(theList.get(2)));
         showNumber3.setVisibility(View.VISIBLE);
     }
 
@@ -117,94 +117,81 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         TextView showNumber6 = findViewById(R.id.whereNumberShows6);
         TextView showBankLast = findViewById(R.id.lastBankBall);
         whichGame();
-        if(getGame == 0){
+        if (getGame == 0) {
             theTIME.setVisibility(View.GONE);
             theDAY.setVisibility(View.GONE);
-            Toast.makeText(this,"Please select a game to continue",
+            Toast.makeText(this, "Please select a game to continue",
                     Toast.LENGTH_LONG).show();
         } else if (getGame == 1) {
-            theList = new gameNumbers().setPick3();
+            theList = new GameNumbers().setPick3();
             showFirstThree();
             gameDays = "Everyday";
             gameTimes = "1:59 PM and 11:00 PM";
+            theDAY.setVisibility(View.VISIBLE);
+            theTIME.setVisibility(View.VISIBLE);
 
 
-        }else if (getGame == 2) {
-            theList = new gameNumbers().setPick4();
+        } else if (getGame == 2) {
+            theList = new GameNumbers().setPick4();
             showFirstThree();
             gameDays = "Everyday";
             gameTimes = "1:59 PM and 11:00 PM";
-            int fourthNumber = theList.get(3);
-            showNumber4.setText(String.valueOf(fourthNumber));
+            showNumber4.setText(String.valueOf(theList.get(3)));
             showNumber4.setVisibility(View.VISIBLE);
 
-        }else if (getGame == 3) {
-            theList = new gameNumbers().setCash5();
+        } else if (getGame == 3) {
+            theList = new GameNumbers().setCash5();
             showFirstThree();
             gameDays = "Everyday";
             gameTimes = "1:59 PM and 11:00 PM";
-            int fourthNumber = theList.get(3);
-            showNumber4.setText(String.valueOf(fourthNumber));
+            showNumber4.setText(String.valueOf(theList.get(3)));
             showNumber4.setVisibility(View.VISIBLE);
-            int fifthNumber = theList.get(4);
-            showNumber5.setText(String.valueOf(fifthNumber));
+            showNumber5.setText(String.valueOf(theList.get(4)));
             showNumber5.setVisibility(View.VISIBLE);
 
-        }else if(getGame == 4) {
-            theList = new gameNumbers().setPowerBall();
+        } else if (getGame == 4) {
+            theList = new GameNumbers().setPowerBall();
             showFirstThree();
             gameDays = "Wednesday and Saturday";
             gameTimes = "11:00 PM";
-            int fourthNumber = theList.get(3);
-            showNumber4.setText(String.valueOf(fourthNumber));
+            showNumber4.setText(String.valueOf(theList.get(3)));
             showNumber4.setVisibility(View.VISIBLE);
-            int fifthNumber = theList.get(4);
-            showNumber5.setText(String.valueOf(fifthNumber));
+            showNumber5.setText(String.valueOf(theList.get(4)));
             showNumber5.setVisibility(View.VISIBLE);
-            int sixthNumber = theList.get(5);
-            showNumber6.setText(String.valueOf(sixthNumber));
+            showNumber6.setText(String.valueOf(theList.get(5)));
             showNumber6.setVisibility(View.VISIBLE);
-        }else if(getGame==5){
-            theList = new gameNumbers().setMegaMillion();
+        } else if (getGame == 5) {
+            theList = new GameNumbers().setMegaMillion();
             showFirstThree();
             gameDays = "Tuesday and Friday";
             gameTimes = "11:00 PM";
-            int fourthNumber = theList.get(3);
-            showNumber4.setText(String.valueOf(fourthNumber));
+            showNumber4.setText(String.valueOf(theList.get(3)));
             showNumber4.setVisibility(View.VISIBLE);
-            int fifthNumber = theList.get(4);
-            showNumber5.setText(String.valueOf(fifthNumber));
+            showNumber5.setText(String.valueOf(theList.get(4)));
             showNumber5.setVisibility(View.VISIBLE);
-            int sixthNumber = theList.get(5);
-            showNumber6.setText(String.valueOf(sixthNumber));
+            showNumber6.setText(String.valueOf(theList.get(5)));
             showNumber6.setVisibility(View.VISIBLE);
-        }else if(getGame==6){
-            theList = new gameNumbers().setCash4Life();
+        } else if (getGame == 6) {
+            theList = new GameNumbers().setCash4Life();
             showFirstThree();
             gameDays = "Monday and Thursday";
             gameTimes = "9:00 PM";
-            int fourthNumber = theList.get(3);
-            showNumber4.setText(String.valueOf(fourthNumber));
+            showNumber4.setText(String.valueOf(theList.get(3)));
             showNumber4.setVisibility(View.VISIBLE);
-            int fifthNumber = theList.get(4);
-            showNumber5.setText(String.valueOf(fifthNumber));
+            showNumber5.setText(String.valueOf(theList.get(4)));
             showNumber5.setVisibility(View.VISIBLE);
-            int sixthNumber = theList.get(5);
-            showNumber6.setText(String.valueOf(sixthNumber));
+            showNumber6.setText(String.valueOf(theList.get(5)));
             showNumber6.setVisibility(View.VISIBLE);
-        }else if(getGame==7){
-            theList = new gameNumbers().setBankAmillion();
+        } else if (getGame == 7) {
+            theList = new GameNumbers().setBankAmillion();
             showFirstThree();
             gameDays = "Wednesday and Saturday";
             gameTimes = "11:00 PM";
-            int fourthNumber = theList.get(3);
-            showNumber4.setText(String.valueOf(fourthNumber));
+            showNumber4.setText(String.valueOf(theList.get(3)));
             showNumber4.setVisibility(View.VISIBLE);
-            int fifthNumber = theList.get(4);
-            showNumber5.setText(String.valueOf(fifthNumber));
+            showNumber5.setText(String.valueOf(theList.get(4)));
             showNumber5.setVisibility(View.VISIBLE);
-            int sixthNumber = theList.get(5);
-            showBankLast.setText(String.valueOf(sixthNumber));
+            showBankLast.setText(String.valueOf(theList.get(5)));
             showBankLast.setVisibility(View.VISIBLE);
         }
     }
